8f45b4c61bc9910739225cbf08aacffae3d3fe9f
Allan Henrique <henriquemeireles129@gmail.com>
2023-12-06 15:20:30 -0300

    Adicionando pacotes npm p/ entrada de dados

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-wasd
+// desafio da DIO 
+
+const entrada = require('prompt-sync')({sigint: true});
+
+let nomeHeroi;
+
+nomeHeroi = entrada("Digite o nome do seu Herói: "); 
+console.log(nomeHeroi); 
+
